8c9e0ec2a4f583adbfaf555e1622d56e48cbb910
nikhuff <nik.huff@me.com>
2018-02-13 12:42:19 -0700

    added routing

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { PageTemplatesComponent } from './page-templates.component';
+import { WebFeaturesComponent } from './web-features.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      { path: 'page-templates', component: PageTemplatesComponent },
+      { path: 'web-features', component: WebFeaturesComponent }
+    ]) 
   ],
   declarations: [PageTemplatesComponent]
 })
