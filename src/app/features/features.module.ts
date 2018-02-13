5ea39eda179ef3826b7297282689e59bf2546870
nikhuff <nik.huff@me.com>
2018-02-13 12:22:30 -0700

    Added page templates component

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PageTemplatesComponent } from './page-templates.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [PageTemplatesComponent]
 })
 export class FeaturesModule { }
