47fb6595d1955323cbcc19f18477808294987954
nikhuff <nik.huff@me.com>
2018-02-27 08:07:49 -0700

    added basic server to server built app

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+const port = process.env.PORT || '1820';
+app.set('port');
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('Listening on port ' + port));
