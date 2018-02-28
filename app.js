da8860ffc932d2dd5c4ebf900cc0528542acf44c
nikhuff <nik.huff@me.com>
2018-02-28 11:29:39 -0700

    fixed server to display app in canvas

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
-const express = require('express');
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
+/*eslint-env node, es6*/
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var ltiMiddleware = require("./express-ims-lti");
+var session = require('express-session');
+var https = require('https');
+var fs = require('fs');
+var index = require('./routes/index');
+var app = express();
 
-const debug = require('debug')('byui-design-lti:server');
-const ltiMiddleware = require('./express-ims-lti');
-const router = require('./routes/index');
+//for local dev faux https
+if (!process.env.URL) {
+  https.createServer({
+    pfx: fs.readFileSync('crt/crt.pfx'),
+    passphrase: 'byuicontent'
+  }, app).listen(1820)
+}
 
-const app = express();
+// view engine setup
+// app.set('views', path.join(__dirname, 'dist'));
+// app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//pretty logs
+app.use(logger('dev'));
+
+//parses in comeing requests to JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+//Cookie Handler
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+// LTI middleware for use
+/********   This middleware and ims-lti dependency do not work with content-item,
+Ben made his own fixes as a work-around, and pulled them out of node modules to push **********/
 
 //This handles the incomeing LTI Launch posts and auth
 app.use(ltiMiddleware({
@@ -18,90 +51,28 @@ app.use(ltiMiddleware({
   consumer_secret: process.env.LTI_SECRET
 }));
 
-//pretty logs
-app.use(logger('dev'));
-
-//parses incoming requests to JSON
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist'));
-});
-
-const port = process.env.PORT || '1820';
-app.set('port', port);
-
-// angular app is served from dist
+//tells express what path to serve from
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use('/', router);
+app.use('/', index);
 
 // catch 404 and forward to error handler
-// app.use((req, res, next) => {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handler
-// app.use((err, req, res, next) => {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.status =  err.status;
-//   res.locals.stack =  err.stack;
-//   //res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.status =  err.status;
+  res.locals.stack =  err.stack;
+  //res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.end(err.message);
-// });
+  // render the error page
+  res.status(err.status || 500);
+  // res.render('error');
+});
 
-// for local dev faux https
-if (!process.env.URL) {
-  server = https.createServer({
-    pfx: fs.readFileSync('crt/crt.pfx'),
-    passphrase: 'byuicontent'
-  }, app).listen(1820)
-} else {
-  const server = http.createServer(app);
-}
-
-server.listen(port, () => console.log('Listening on port ' + port));
-server.on('error', onError);
-server.on('listening', onListening);
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
+module.exports = app;
