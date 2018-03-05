f38f99656baf4b5f35b01589c16079304d2651fc
--global <--global>
2018-03-05 11:12:04 -0700

    changed columns to component

---
+++ src/app/features/web-features/previews/columns-preview.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-columns-preview',
+  template: `
+    <div class="content-box">
+      <div class="grid-row">
+          <div class="col-xs-6" *ngFor="let item of array(numberOfColumns)">
+              <ul>
+                  <li>Lorem ipsum dolor</li>
+                  <li>Lorem ipsum dolor</li>
+              </ul>
+          </div>
+      </div>
+    </div>
+  `,
+  styles: []
+})
+export class ColumnsPreviewComponent implements OnInit {
+
+  @Input() numberOfColumns: number;
+
+  constructor() { }
+
+  array(value: number): any[] {
+    const num = Number(value);
+    return Array.from(Array(num).keys());
+  }
+
+  ngOnInit() {
+  }
+
+}
