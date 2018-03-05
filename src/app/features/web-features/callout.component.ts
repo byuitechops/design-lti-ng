87a07ec9e805983f841ade7744a3383992256d69
--global <--global>
2018-03-05 13:47:30 -0700

    added emitter to insert callout button

--- src/app/features/web-features/callout.component.ts
+++ src/app/features/web-features/callout.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
+import { CalloutPreviewComponent } from './previews/callout-preview.component';
 
 @Component({
   selector: 'app-callout',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class CalloutComponent implements OnInit {
+
+  @Output() select: EventEmitter<string> = new EventEmitter();
+  @ViewChild(CalloutPreviewComponent) preview: CalloutPreviewComponent;
 
   positions: string[] = ['left', 'center', 'right'];
   sizes: string[] = ['full', 'half', 'quarter'];
@@ -29,6 +33,12 @@ export class CalloutComponent implements OnInit {
 
   changeCalloutSize(value: string): void {
     this.calloutSize = value;
+  }
+
+  chooseFeature() {
+    const html = this.preview.getHtmlContent().trim();
+    console.log(html);
+    this.select.emit(html);
   }
 
   ngOnInit() {
