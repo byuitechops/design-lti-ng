438984f4001017a3b732bf3eb8a50f25f8d58925
nikhuff <nik.huff@me.com>
2018-03-06 09:09:28 -0700

    fixed insert popover button

--- src/app/features/web-features/previews/popover-preview.component.ts
+++ src/app/features/web-features/previews/popover-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-popover-preview',
@@ -14,7 +14,15 @@ export class PopoverPreviewComponent implements OnInit {
   @Input() popoverBaseText: string;
   @Input() popoverText: string;
 
-  constructor() { }
+  elementRef: ElementRef;
+
+  constructor(elementRef: ElementRef) {
+    this.elementRef = elementRef;
+  }
+
+  getHtmlContent() {
+    return this.elementRef.nativeElement.innerHTML;
+  }
 
   ngOnInit() {
   }
