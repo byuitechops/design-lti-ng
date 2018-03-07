ba28ab6a6b5871ca16b9408599d186e1f98ee1ac
--global <--global>
2018-03-07 13:21:10 -0700

    changed factory to ng componentoutlet

--- src/app/features/feature-wrapper.component.ts
+++ src/app/features/feature-wrapper.component.ts
@@ -1,50 +1,23 @@
 import { NgModule, Component, Compiler, ViewContainerRef, ViewChild, Input,
-  ComponentRef, ComponentFactory, ComponentFactoryResolver, ChangeDetectorRef, OnChanges, AfterViewInit, OnDestroy } from '@angular/core';
+  ComponentRef, ComponentFactory, ComponentFactoryResolver, ChangeDetectorRef,
+  OnChanges, AfterViewInit, OnDestroy, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-feature-wrapper',
   template: `
-    <div #target></div>
+    <ng-container *ngComponentOutlet="type" (select)="pass($event)" ngOutletInjector=""></ng-container>
   `,
   styles: []
 })
-export class FeatureWrapperComponent implements OnChanges, AfterViewInit, OnDestroy {
-  @ViewChild('target', { read: ViewContainerRef }) target;
+export class FeatureWrapperComponent {
+
   @Input() type;
-  cmpRef: ComponentRef<any>;
-  private isViewInitialized = false;
+  @Output() select: EventEmitter<string> = new EventEmitter();
 
-  constructor(private componentFactoryResolver: ComponentFactoryResolver, private compiler: Compiler,
-    private cdRef: ChangeDetectorRef) { }
+  constructor() { }
 
-    updateComponent() {
-      if (!this.isViewInitialized) {
-        return;
-      }
-      if (this.cmpRef) {
-        this.cmpRef.destroy();
-      }
-
-      const factory = this.componentFactoryResolver.resolveComponentFactory(this.type);
-      this.cmpRef = this.target.createComponent(factory);
-      // to access the created instance use
-      // this.compRef.instance.someProperty = 'someValue';
-      // this.compRef.instance.someOutput.subscribe(val => doSomething());
-      this.cdRef.detectChanges();
-    }
-
-    ngOnChanges() {
-      this.updateComponent();
-    }
-
-    ngAfterViewInit() {
-      this.isViewInitialized = true;
-      this.updateComponent();
-    }
-
-    ngOnDestroy() {
-      if (this.cmpRef) {
-        this.cmpRef.destroy();
-      }
-    }
+  pass(feature) {
+    console.log('pass');
+    this.select.emit(feature);
+  }
 }
