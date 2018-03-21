104552547c222a396fa1cece0febcfa24b522311
Huff <nikhuff@byui.edu>
2018-03-21 12:20:35 -0600

    inserting page templates is functional

--- src/app/features/templates/org-template.component.ts
+++ src/app/features/templates/org-template.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, ViewChild, EventEmitter } from '@angular/core';
 import { GenericActivityComponent } from './previews/generic-activity.component';
+import { GenericQuizComponent } from './previews/generic-quiz.component';
 
 @Component({
   selector: 'app-org-template',
@@ -11,7 +12,8 @@ export class OrgTemplateComponent implements OnInit {
   @Input() courseClass: string;
 
   @Output() select: EventEmitter<string> = new EventEmitter();
-  @ViewChild(GenericActivityComponent) preview: GenericActivityComponent;
+  @ViewChild(GenericActivityComponent) activity: GenericActivityComponent;
+  @ViewChild(GenericQuizComponent) quiz: GenericQuizComponent;
 
   bannerInner: string;
   selectedBanner: string;
@@ -34,6 +36,18 @@ export class OrgTemplateComponent implements OnInit {
     this.selectedBanner = value;
     this.bannerInner = this.selectedBanner.split(' ')[1];
     console.log(this.selectedBanner);
+  }
+
+  chooseFeature() {
+    let html: string;
+    if (this.selectedTemplate === 'genericActivity') {
+      html = this.activity.getHtmlContent().trim();
+    } else if (this.selectedTemplate === 'genericQuiz') {
+      html = this.quiz.getHtmlContent().trim();
+    }
+
+    console.log(html);
+    this.select.emit(html);
   }
 
   ngOnInit() {
