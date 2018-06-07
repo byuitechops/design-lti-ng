707a155610c03951063f3814f63f4b6589d5075e
nikhuff <nik.huff@me.com>
2018-06-07 13:16:11 -0600

    more changes on video component

--- src/app/features/web-features/video.component.ts
+++ src/app/features/web-features/video.component.ts
@@ -13,27 +13,27 @@ export class VideoComponent implements OnInit {
   endTime: string;
 
   noAudio = false;
-  htmlTranscript: string;
-  rtfTranscript: string;
+  htmlTranscript = 'https://content.byui.edu/file/f0e0353b-0473-4808-9cfb-6ca04d0b0020/1/no-transcript.html';
+  rtfTranscript = 'https://content.byui.edu/file/f0e0353b-0473-4808-9cfb-6ca04d0b0020/1/no-transcript.html';
 
-  type: string = 'kaltura';
-  size: string = 'medium';
-  aspect: string = '16-9';
+  type = 'kaltura';
+  size = 'medium';
+  aspect = '16-9';
 
-  link: boolean = false;
-  download: boolean = false;
+  link = false;
+  download = false;
   height: number;
   width: number;
-  
+
   embedcode = '<div class="byui-video" data-id"';
   output: string;
   videoid: string;
-  
+
   constructor() { }
 
   ngOnInit() {
   }
-  
+
   updateType(type: string) {
     this.type = type;
   }
@@ -44,6 +44,42 @@ export class VideoComponent implements OnInit {
 
   updateAspectRatio(aspect: string) {
     this.aspect = aspect;
+  }
+
+  updateTranscripts() {
+    this.noAudio = !this.noAudio;
+    if (this.noAudio) {
+      this.htmlTranscript = 'https://content.byui.edu/file/f0e0353b-0473-4808-9cfb-6ca04d0b0020/1/no-audio.html';
+      this.rtfTranscript = 'https://content.byui.edu/file/f0e0353b-0473-4808-9cfb-6ca04d0b0020/1/no-audio.html';
+    } else {
+      this.htmlTranscript = 'https://content.byui.edu/file/f0e0353b-0473-4808-9cfb-6ca04d0b0020/1/no-transcript.html';
+      this.rtfTranscript = 'https://content.byui.edu/file/f0e0353b-0473-4808-9cfb-6ca04d0b0020/1/no-transcript.html';
+    }
+  }
+
+  convert() {
+    switch (this.type) {
+      case 'youtube':
+          this.videoid = this.url.split('v=').pop();
+          break;
+      case 'kaltura':
+          this.videoid = this.url.split('/').pop();
+          break;
+      default:
+          break;
+    }
+
+    this.embedcode += this.videoid + '" data-platform="'
+                   + this.type + '" data-size="'
+                   + this.size + '" data-ratio="'
+                   + this.aspect + '" data-transcriptHTML="'
+                   + this.htmlTranscript + '" data-transcriptRTF="'
+                   + this.rtfTranscript + '" data-link="'
+                   + this.link + '" data-download="'
+                   + this.download + '" data-title="'
+                   + this.title + '" data-starttime="'
+                   + this.startTime + '" data-endtime="'
+                   + this.endTime + '">"';
   }
 
   test() {
