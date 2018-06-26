87bc0c95d8f63380ab1a90f3e4cf16ce82b43430
nikhuff <nik.huff@me.com>
2018-06-26 16:10:01 -0600

    fixed dialogs

--- src/app/features/web-features/previews/dialog-preview.component.ts
+++ src/app/features/web-features/previews/dialog-preview.component.ts
@@ -3,11 +3,11 @@ import { Component, OnInit, Input, ElementRef } from '@angular/core';
 @Component({
   selector: 'app-dialog-preview',
   template: `
-<div id="dialog_for_link_1" title="dialogTitle ? dialogTitle : Title" class="enhanceable_content dialog">
+<div id="dialog_for_link_{{date}}" title="{{dialogTitle ? dialogTitle : 'Title'}}" class="enhanceable_content dialog">
   {{dialogContent ? dialogContent : "dialog"}}
 </div>
 <p>
-    <a id="link_1" class="Button">
+    <a class="Button" href="#dialog_for_link_{{date}}" id="{{date}}">
       {{dialogPrompt ? dialogPrompt : "prompt"}}
     </a>
 </p>
@@ -18,6 +18,9 @@ export class DialogPreviewComponent implements OnInit {
 
   @Input() dialogContent: string;
   @Input() dialogPrompt: string;
+  @Input() dialogTitle: string;
+
+  date: number;
 
   elementRef: ElementRef;
 
@@ -30,6 +33,7 @@ export class DialogPreviewComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.date = new Date().getTime();
   }
 
 }
