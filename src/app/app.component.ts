384b044b8cde1fcac060daa6775aedb403790647
Danverde <gre12014@byui.edu>
2018-08-22 12:09:57 -0600

    fixed broken web feature anchor links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,22 +11,24 @@ export class AppComponent {
   selected = 'pageTemplates';
 
   _headingText: string;
+
   get headingText(): string {
     return this._headingText;
   }
+
   set headingText(value: string) {
     this._headingText = value;
   }
 
   constructor(router: Router) {
     router.events.subscribe(s => {
-        if (s instanceof NavigationEnd) {
-            const tree = router.parseUrl(router.url);
-            if (tree.fragment) {
-                const element = document.querySelector('#' + tree.fragment);
-                if (element) { element.scrollIntoView(true); }
-            }
+      if (s instanceof NavigationEnd) {
+        const tree = router.parseUrl(router.url);
+        if (tree.fragment) {
+          const element = document.querySelector('#' + tree.fragment);
+          if (element) { element.scrollIntoView(true); }
         }
+      }
     });
   }
 
