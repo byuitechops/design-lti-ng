86043fe28697d2395ad9d3568ef4422b367d8ae3
Aaron Shiffler <ajshiffler@gmail.com>
2018-12-14 15:28:17 -0700

    commented out unused routes

--- routes/index.js
+++ routes/index.js
@@ -6,41 +6,41 @@ var fs = require('fs');
 
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('*', function (req, res, next) {
     res.sendFile(path.join(__dirname, '../dist/index.html'), {
         title: 'Express'
     });
 });
 
-router.route('/api/course-name').get((req, res) => {
-    var ltiParams = req.session.lti.params;
-    var courseName = ltiParams.context_title;
-});
+// router.route('/api/course-name').get((req, res) => {
+//     var ltiParams = req.session.lti.params;
+//     var courseName = ltiParams.context_title;
+// });
 
-router.route('/api/course-class').get((req, res) => {
-    var ltiParams = req.session.lti.params;
-    var courseClass = ltiParams.context_label.toLowerCase().replace(' ', '');
-    res.send(JSON.stringify(courseClass));
-});
+// router.route('/api/course-class').get((req, res) => {
+//     var ltiParams = req.session.lti.params;
+//     var courseClass = ltiParams.context_label.toLowerCase().replace(' ', '');
+//     res.send(JSON.stringify(courseClass));
+// });
 
-router.route('/api/course-id').get((req, res) => {
-    var ltiParams = req.session.lti.params;
-    var courseNumber = ltiParams.content_item_return_url.split('/')[4];
-    res.send(JSON.stringify(courseNumber));
-});
+// router.route('/api/course-id').get((req, res) => {
+//     var ltiParams = req.session.lti.params;
+//     var courseNumber = ltiParams.content_item_return_url.split('/')[4];
+//     res.send(JSON.stringify(courseNumber));
+// });
 
-router.route('/api/return-url').get((req, res) => {
-    var ltiParams = req.session.lti.params;
-    var returnUrl = ltiParams.content_item_return_url;
-    console.log(returnUrl);
-    res.send(JSON.stringify(returnUrl));
-});
+// router.route('/api/return-url').get((req, res) => {
+//     var ltiParams = req.session.lti.params;
+//     var returnUrl = ltiParams.content_item_return_url;
+//     console.log(returnUrl);
+//     res.send(JSON.stringify(returnUrl));
+// });
 
-router.route('/api').get((req, res) => {
-    var ltiParams = req.session.lti.params;
-    console.log(ltiParams);
-    res.send(JSON.stringify(ltiParams));
-});
+// router.route('/api').get((req, res) => {
+//     var ltiParams = req.session.lti.params;
+//     console.log(ltiParams);
+//     res.send(JSON.stringify(ltiParams));
+// });
 
 /* Handle LTI launch */
 router.post('/', function (req, res, next) {
